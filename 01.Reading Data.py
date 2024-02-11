a540671fa4af8b84fd487a9afbcd5c5767fa5ffe
chrknov6@hotmail.com <chrknov6@hotmail.com>
2024-02-11 08:08:01 +0000

    Changes1

--- 01.Reading Data.py
+++ 01.Reading Data.py
@@ -1,5 +1,6 @@
 # Databricks notebook source
 df = spark.read.options(header = True).csv("abfss://raw@storageadlsdatabricks.dfs.core.windows.net/Countries/countries.csv")
+df.display()
 
 # COMMAND ----------
 
